24beed9ef1b6042e2124035da1436c72881ad123
Jesse Schimmel <jesseschimmel@Jesses-MacBook-Pro.local>
2019-09-10 18:52:05 -0400

    Updated linter and views main

--- config/config.js
+++ config/config.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.PASSWORD,
-    "database": "exampledb",
-    "host": "localhost",
-    "dialect": "mysql"
+  development: {
+    username: "root",
+    password: process.env.PASSWORD,
+    database: "exampledb",
+    host: "localhost",
+    dialect: "mysql"
   },
-  "test": {
-    "username": "root",
-    "password": process.env.PASSWORD,
-    "database": "testdb",
-    "host": "localhost",
-    "dialect": "mysql",
-    "logging": false
+  test: {
+    username: "root",
+    password: process.env.PASSWORD,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
   },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
   }
-}
+};
